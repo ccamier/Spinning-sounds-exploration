e5bcc2092dd77d0a6ec38900db489776ae6558b4
ccamier <cedric.camier@gmail.com>
2022-05-02 16:57:28 +0200

    Add files via upload

--- streamlit_test_app.py
+++ streamlit_test_app.py
@@ -7,5 +7,20 @@ Created on Mon May  2 15:04:42 2022
 
 import streamlit as st
 
+import wave, struct, random
+sampleRate = 44100.0 # hertz
+#duration = 1.0 # seconds
+#frequency = 440.0 # hertz
+obj = wave.open('sound.wav','w')
+obj.setnchannels(1) # mono
+obj.setsampwidth(2)
+obj.setframerate(sampleRate)
+for i in range(99999):
+   value = random.randint(-32767, 32767)
+   data = struct.pack('<h', value)
+   obj.writeframesraw( data )
+obj.close()
+
 st.header('Test Cédric')
 st.subheader('coucou')
+st.audio('sound.wav',format='wav')
